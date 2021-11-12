b7c02b6590473222470d85b53b42f9a3fb32e0df
siaochanwu <jill007tw@gmail.com>
2021-11-12 09:19:23 +0800

    setting deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: 'tourism_web'  //對應github 項目名稱
 })
